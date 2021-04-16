11c4ee203eb4fe8c4033d154a7ace89167c18072
Parker Sequeira <phsequeira@yahoo.com>
2021-04-16 09:24:47 -0700

    started post model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS users CASCADE; 
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
     gh_username TEXT NOT NULL PRIMARY KEY,
     gh_avatar TEXT NOT NULL
 );
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    photo_url TEXT NOT NULL,
+    caption TEXT NOT NULL,
+    user TEXT REFERENCES users(gh_username)
+)
