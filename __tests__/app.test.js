b4d6fb6f6245fdc96a9c666bb075f01233b24896
Parker Sequeira <phsequeira@yahoo.com>
2021-04-16 17:39:50 -0700

    writing test for comment

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,15 +4,16 @@ const request = require('supertest');
 const app = require('../lib/app');
 const User = require('../lib/models/user');
 const Post = require('../lib/models/Post');
+const Comment = require('../lib/models/comment');
 
 jest.mock('../lib/middleware/checkAuth.js', () => (req, res, next) => {
   req.user = {
-    gh_username: 'test',
-    gh_avatar: 'http://image.com/image.png',
+    ghUsername: 'test',
+    ghAvatar: 'http://image.com/image.png',
   };
   next();
 });
-describe('tardygram routes', () => {
+describe('posts routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -60,5 +61,154 @@ describe('tardygram routes', () => {
           username: 'test',
         })
     })
+  })
+  it('gets a post by id', async () => {
+    await User.createUser({
+      ghUsername: 'test',
+      ghAvatar: 'http://image.com/image.png',
+    });
+    await Post.createPost(
+      {
+        photo_url: 'http://hellothere.com',
+        caption: 'hi',
+        username: 'test'
+      });
+
+      return request(app)
+        .get('/api/v1/post/1')
+        .then((res) => {
+          expect(res.body).toEqual({
+            id: '1',
+            photo_url: 'http://hellothere.com',
+            caption: 'hi',
+            username: 'test',
+            comment: null,
+            comment_by: null,
+            comment_id: null,
+            gh_avatar: null,
+            gh_username: null,
+            post: null,
+          })
+        })
+  });
+
+  it('updates a post using PATCH', async () => {
+    await User.createUser({
+      ghUsername: 'test',
+      ghAvatar: 'http://image.com/image.png',
+    });
+    await Post.createPost(
+      {
+        photo_url: 'http://hellothere.com',
+        caption: 'hi',
+        username: 'test'
+      });
+
+      return request(app)
+        .patch('/api/v1/post/1')
+        .send({
+          id: 1, 
+          caption: 'updated',
+        })
+        .then((res) => {
+          expect(res.body).toEqual({
+            id: '1',
+            photo_url: 'http://hellothere.com',
+            caption: 'updated',
+            username: 'test',
+          })
+        })
+  })
+
+  it('deletes post by id', async () => {
+    await User.createUser({
+      ghUsername: 'test',
+      ghAvatar: 'http://image.com/image.png',
+    });
+    await Post.createPost(
+      {
+        photo_url: 'http://hellothere.com',
+        caption: 'hi',
+        username: 'test'
+      });
+
+      return request(app)
+        .delete('/api/v1/post/1')
+        .then((res) => {
+          expect(res.body).toEqual({
+            id: '1',
+            photo_url: 'http://hellothere.com',
+            caption: 'hi',
+            username: 'test'
+          })
+        })
+  })
+
+  it('gets top 10 popular post', async () => {
+    await User.createUser({
+      ghUsername: 'test',
+      ghAvatar: 'http://image.com/image.png',
+    });
+    await Post.createPost(
+      {
+        photo_url: 'http://hellothere.com',
+        caption: 'hi',
+        username: 'test'
+      });
+      await Post.createPost(
+        {
+          photo_url: 'http://dog.com',
+          caption: 'dog',
+          username: 'test'
+        });
+
+      return request(app)
+        .get('/api/v1/post/popular')
+        .then((res) => {
+          expect(res.body).toEqual([
+            {
+              id: '2',
+              total_comments: '0'
+            },
+            {
+              id: '1',
+              total_comments: '0'
+            }
+          ])
+        })
+  })
+});
+
+describe('comments routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('adds a comment using POST', async () => {
+    await Comment.createcomment({
+
+    })
+
+    return request(app)
+      .post('/api/v1/comment')
+      .then((res) => {
+        expect(res.body).toEqual({
+
+        })
+      })
+  })
+
+  it('deletes comment', async () => {
+    await Comment.createcomment({
+
+    })
+
+    return request(app)
+      .delete('/api/v1/comment/1')
+      .then((res) => {
+        expect(res.body).toEqual({
+
+        })
+      })
   })
 });
